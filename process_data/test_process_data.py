09fdb9a913caac33e526c9d82ea8017590a4827a
Lukasz Aszyk <lukasz.aszyk@gmail.com>
2021-01-14 01:03:31 +0100

    Update test_process_data.py

--- process_data/test_process_data.py
+++ process_data/test_process_data.py
@@ -3,16 +3,16 @@ from process_data.process_data import load_data, clean_data, save_data
 
 
 def test_load_data_is_returning_two_entities():
-    assert len(load_data('disaster_messages.csv', 'categories.csv'))==2
+    assert len(load_data('/process_data/disaster_messages.csv', 'categories.csv'))==2
 
 
 def test_load_data_is_dataframe():
-    assert isinstance(load_data('disaster_messages.csv', 'disaster_categories.csv')[0], pd.DataFrame), "The messages file doesn't return a dataframe"
-    assert isinstance(load_data('disaster_messages.csv', 'disaster_categories.csv')[1], pd.DataFrame), "The categories file doesn't return a dataframe"
+    assert isinstance(load_data('/process_data/disaster_messages.csv', '/process_data/disaster_categories.csv')[0], pd.DataFrame), "The messages file doesn't return a dataframe"
+    assert isinstance(load_data('/process_data/disaster_messages.csv', '/process_data/disaster_categories.csv')[1], pd.DataFrame), "The categories file doesn't return a dataframe"
 
 
 def test_clean_data_is_dataframe():
-    assert isinstance(clean_data(load_data('disaster_messages.csv', 'disaster_categories.csv')), pd.DataFrame), "The function doesn't return a dataframe"
+    assert isinstance(clean_data(load_data('/process_data/disaster_messages.csv', '/process_data/disaster_categories.csv')), pd.DataFrame), "The function doesn't return a dataframe"
 
 def test_clean_data_is_not_empty_dataframe():
-    assert not clean_data(load_data('disaster_messages.csv', 'disaster_categories.csv')).empty, "The function returns empty dataframe"
+    assert not clean_data(load_data('/process_data/disaster_messages.csv', '/process_data/disaster_categories.csv')).empty, "The function returns empty dataframe"
