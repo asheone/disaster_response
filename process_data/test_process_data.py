131d3a68c52ee037295fcaaae7b6e8383ca2bdff
lukaszaszyk <lukasz.aszyk@gmail.com>
2021-01-14 10:43:07 +0100

    fix

--- process_data/test_process_data.py
+++ process_data/test_process_data.py
@@ -3,7 +3,7 @@ import os
 import os.path
 from process_data.process_data import load_data, clean_data, save_data
 
-directory = os.path()
+directory = os.getcwd()
 
 def test_if_the_file_exsist():
 
@@ -15,16 +15,16 @@ def test_if_the_file_exsist():
           print("File {0} does not exists!!".format(file))
 
 def test_load_data_is_returning_two_entities():
-    assert len(load_data("{0}/disaster_messages.csv".format(directory), "{0}/categories.csv".format(directory)))==2
+    assert len(load_data("/disaster_messages.csv".format(directory), "/categories.csv".format(directory)))==2
 
 
 def test_load_data_is_dataframe():
-    assert isinstance(load_data("{0}/disaster_messages.csv".format(directory), "{0}/disaster_categories.csv".format(directory))[0], pd.DataFrame), "The messages file doesn't return a dataframe"
-    assert isinstance(load_data("{0}/disaster_messages.csv".format(directory), "{0}/disaster_categories.csv".format(directory))[1], pd.DataFrame), "The categories file doesn't return a dataframe"
+    assert isinstance(load_data("/disaster_messages.csv".format(directory), "/disaster_categories.csv".format(directory))[0], pd.DataFrame), "The messages file doesn't return a dataframe"
+    assert isinstance(load_data("/disaster_messages.csv".format(directory), "/disaster_categories.csv".format(directory))[1], pd.DataFrame), "The categories file doesn't return a dataframe"
 
 
 def test_clean_data_is_dataframe():
-    assert isinstance(clean_data(load_data("{0}/disaster_messages.csv".format(directory), "{0}/disaster_categories.csv".format(directory))), pd.DataFrame), "The function doesn't return a dataframe"
+    assert isinstance(clean_data(load_data("/disaster_messages.csv".format(directory), "/disaster_categories.csv".format(directory))), pd.DataFrame), "The function doesn't return a dataframe"
 
 def test_clean_data_is_not_empty_dataframe():
-    assert not clean_data(load_data("{0}/disaster_messages.csv".format(directory), "{0}/disaster_categories.csv".format(directory))).empty, "The function returns empty dataframe"
+    assert not clean_data(load_data("/disaster_messages.csv".format(directory), "/disaster_categories.csv".format(directory))).empty, "The function returns empty dataframe"
